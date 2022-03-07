1a6a25545e23ca4ba2ee04a8eb8a87ad520556e3
John Parks <jcparks1984@gmail.com>
2022-03-07 15:26:40 -0500

    Added Required Files For Project + Removed Unnecessary Files

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { usestate } from 'react'
+
+function App() {
+  return (
+    <div>
+      <h3>Hello World</h3>
+    </div>
+  );
+}
+
+export default App;
